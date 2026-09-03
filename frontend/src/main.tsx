4579a8d4a53ee9b82c92d1aa4931dfd65a307e8c
AdarshaAdi5379 <adarshakk1234@gmail.com>
2026-09-03 23:50:38 +0530

    added vercel analytics feature code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './index.css';
 
@@ -30,6 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           },
         }}
       />
+      <Analytics />
     </QueryClientProvider>
   </React.StrictMode>
 );
